a7c26d0a1a51785c09d947242e5d704374d32dec
VivekDeyVD <vivekvd35@gmail.com>
2024-03-13 22:50:11 +0530

    java practice

---
+++ Greater.java
@@ -0,0 +1,17 @@
+class  Greater{
+
+public static void main(String [] args){
+
+int a=10;
+int b=30;
+
+if(a>b){
+
+System.out.println("a is greater");
+}
+
+else{
+System.out.println("b is greater");
+}
+}
+}
---
+++ great.java
@@ -0,0 +1,22 @@
+class great {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+       
+        int b = 20;
+        int c = 30;
+
+        if (a > b && a > c) {
+            System.out.println("a is greater");
+        } 
+        
+        else if (b > c && b > c) {
+            System.out.println("b is greater");
+        } 
+       
+        else {
+            System.out.println("c is greater");
+        }
+    }
+}
