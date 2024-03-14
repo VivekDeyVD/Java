d4048e0137cefa93edb9fab5e61aefa6b689fe98
VivekDeyVD <vivekvd35@gmail.com>
2024-03-14 18:37:32 +0530

    Leap year program

---
+++ leapYear.java
@@ -0,0 +1,13 @@
+class leapYear{
+public static void main(String [] args){
+int year =2025;
+
+if(year%4==0){
+System.out.println("Leap Year");
+}
+
+else{
+System.out.println("Not a Leap year");
+}
+}
+}
