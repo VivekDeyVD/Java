dfefdad8bdb7e83d2118725b99faa6cc45c5a098
VivekDeyVD <vivekvd35@gmail.com>
2024-03-14 18:36:33 +0530

    even odd program

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+class EvenOdd
+{
+	public static void main(String [] args)
+	{
+		int a=9;
+		
+		if(a%2==0)
+		{
+			System.out.println("number is even");
+		}
+		else 
+		{
+			System.out.println("number is odd");
+		}
+	}
+}
