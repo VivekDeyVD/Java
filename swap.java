706582d01e693d820910812337d0c6b2af9fea07
VivekDeyVD <vivekvd35@gmail.com>
2024-03-14 18:38:09 +0530

    Swap program

---
+++ swap.java
@@ -0,0 +1,16 @@
+class swap 
+{
+	public static void main(String [] args)
+	{
+		int a=10;
+		int b=20;
+		System.out.println("Numbers Before Swapping :" + a + " "+ b);
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("Numbers After Swapping :" + a + " "+ b);
+
+		
+	}
+}
