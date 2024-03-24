fa87772a6a5b4bb680574735fe73e359135d49a7
VivekDeyVD <vivekvd35@gmail.com>
2024-03-24 19:36:26 +0530

    pattern type 4

---
+++ pattern4.java
@@ -0,0 +1,9 @@
+class pattern4{
+    public static void main(String[] args){
+        for(int i=0;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                
+            }
+        }
+    }
+}
