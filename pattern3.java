702fe9d049c2857fcb97e9e82b2da303ad09b043
VivekDeyVD <vivekvd35@gmail.com>
2024-03-24 19:35:58 +0530

    pattern type 3

---
+++ pattern3.java
@@ -0,0 +1,19 @@
+class pattern3{
+    public static void main(String[] args){
+        for(int i=0;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+                
+            }
+            System.out.println();
+        }
+      
+        for(int i=4; i>=1; i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+              
+            }
+            System.out.println();
+        }
+    }
+}
