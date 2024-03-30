7594b021bf80d8e315c7f63b0d577f6fb0377ff2
VivekDeyVD <vivekvd35@gmail.com>
2024-03-30 23:33:37 +0530

    pattern 8

--- pattern8.java
+++ pattern8.java
@@ -1,5 +1,63 @@
-class pattern8{
-    public static void main(String[] args){
-        
+// Java Program to print
+// Butterfly Pattern
+import java.util.*;
+ 
+// Java code for printing pattern
+public class GeeksForGeeks {
+ 
+    // Function to demonstrate pattern
+    public static void printPattern(int n)
+    {
+        int i, j;
+        int num = 1;
+ 
+        // outer loop to handle upper part
+        for (i = 1; i <= n; i++) {
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+ 
+            // inner loop to print spaces
+            int spaces = 2 * (n - i);
+            for (j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+ 
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+ 
+            System.out.println();
+        }
+ 
+        // outer loop to handle lower part
+        for (i = n; i >= 1; i--) {
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+ 
+            // inner loop to print spaces
+            int spaces = 2 * (n - i);
+            for (j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+ 
+            // inner loop to print stars
+            for (j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+ 
+            System.out.println();
+        }
+    }
+ 
+    // Driver Function
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
     }
 }
