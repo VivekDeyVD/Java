c2e47ed462672d14de04382ef113caedad6de7cd
VivekDeyVD <vivekvd35@gmail.com>
2024-03-31 16:29:30 +0530

    Insertion Sort

--- insertionSort.java
+++ insertionSort.java
@@ -1,5 +1,20 @@
 class insertion {
     public static void main(String [] args){
-        
+        int[] a={5,1,6,2,4,3};
+        int temp,j;
+        for (int i=1;i<a.length;i++){
+
+            temp=a[i];
+            j=i;
+            while(j>0 && a[j-1]>temp){
+                a[j]=a[j-1];
+                j=j-1;
+            }
+            a[j]=temp;
+
+        }
+        for(int i=0 ; i<a.length;i++){
+            System.out.print(a[i] + " ");
+        }
     }
 }
