1c34cf59bf5e1cd80cc01a1d69840bbcf8536358
VivekDeyVD <vivekvd35@gmail.com>
2024-04-27 10:58:12 +0530

    duplicate element brute force method

---
+++ Duplicate.java
@@ -0,0 +1,14 @@
+class Duplicate {
+    public static void main(String[] args){
+        //brute force
+        System.out.println("the duplicate elements are :");
+        int a[]={5,4,3,2,2,1,3};
+        for(int i=0;i<a.length-1;i++){
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]==a[j] && (i!=j)){
+System.out.println(a[j]+ " ");
+                }
+            }
+        }
+    }
+}
