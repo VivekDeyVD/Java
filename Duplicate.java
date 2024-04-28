da173a8d044c4a09c46b43447b864f35f2017355
VivekDeyVD <vivekvd35@gmail.com>
2024-04-28 19:18:56 +0530

    Committing changes before merge

--- Duplicate.java
+++ Duplicate.java
@@ -1,7 +1,7 @@
 class Duplicate {
     public static void main(String[] args){
         //brute force
-        System.out.println("the duplicate elements are :");
+        System.out.println("the duplicate elements are :"); 
         int a[]={5,4,3,2,2,1,3};
         for(int i=0;i<a.length-1;i++){
             for(int j=i+1;j<a.length;j++){
