6046059f72446c9f27e93afdeee1e54351f3c9c0
VivekDeyVD <vivekvd35@gmail.com>
2024-04-30 08:48:33 +0530

    first duplicate using brute force method

---
+++ FirstDuplicate.java
@@ -0,0 +1,21 @@
+class FirstDuplicate{
+    public static void main(String[] args){
+        int a[]={6,5,3,2,5,2,4};
+        //brute force
+        int temp=0;
+        for(int i=0;i<a.length-1;i++){
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]==a[j]){
+                    System.out.println( "first duplicate element is   " + a[i]);
+                    temp+=1;
+                    break;
+
+                }
+
+            }
+            if(temp>0){
+                break;
+            }
+        }
+    }
+}
