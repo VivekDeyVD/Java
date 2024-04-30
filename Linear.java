f433e90644fc5ce46b61a7ac864ffe59ea60da2d
VivekDeyVD <vivekvd35@gmail.com>
2024-04-30 09:13:18 +0530

    pattern 8

--- Linear.java
+++ Linear.java
@@ -1,4 +1,4 @@
-public class LinearSearchExample{    
+public class Linear{    
 public static int linearSearch(int[] arr, int key){    
         for(int i=0;i<arr.length;i++){    
             if(arr[i] == key){    
@@ -11,3 +11,5 @@ public static int linearSearch(int[] arr, int key){
         int[] a1= {10,20,30,50,70,90};    
         int key = 50;    
         System.out.println(key+" is found at index: "+linearSearch(a1, key));
+    }
+}
