326048b9f7b3afb1d5e1f425cffe4fe4c23af77a
VivekDeyVD <vivekvd35@gmail.com>
2024-05-01 08:02:07 +0530

    finding common element in two array usinng hash set

--- CommonElement.java
+++ CommonElement.java
@@ -1,14 +1,20 @@
+import java.util.HashSet;
+
 class CommonElement{
     public static void main(String [] args){
-        int a1[]={1,3,7,9,2};
+        int a1[]={1,3,7,9,3,2};
         int a2[]={5,1,4,8,3};
-
+        HashSet<Integer> hs = new HashSet<>();
         for(int i=0;i<a1.length;i++){
             for(int j=0;j<a2.length;j++){
                 if(a1[i]==a2[j]){
-                    System.out.println("commonelement is : " + a1[i]);
+                   hs.add(a1[i]);
+                   break;
                 }
             }
+        }
+        for(int no:hs){
+            System.out.println(no+" ");
         }
 
     }
