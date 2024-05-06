2e4961b11f83ccaaf19dbd58c32bf14f4afd620e
VivekDeyVD <vivekvd35@gmail.com>
2024-05-06 20:13:52 +0530

    ocuurence

--- OneOccurence.java
+++ OneOccurence.java
@@ -2,6 +2,11 @@ class OneOccurence{
     public static void main(String[] args){
         int a[]={5,3,4,5,2,3,4};
         //uisng XOR
-        
+        int res=a[0];
+        for(int i=1;i<a.length;i++){
+            res=res^a[i];
+
+        }
+        System.out.println(res);
     }
 }
